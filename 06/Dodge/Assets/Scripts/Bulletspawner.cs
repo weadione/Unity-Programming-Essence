7a7b1e00e0a6cb2e77b366ca92ee7322cedc860e
weadione <36260010+weadione@users.noreply.github.com>
2020-12-17 21:22:12 +0900

    닷지 만듦

---
+++ 06/Dodge/Assets/Scripts/Bulletspawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulletspawner : MonoBehaviour
+{
+    public GameObject bulletPrefeb;
+    public float spawnRateMin = 0.5f;
+    public float spawnRateMax = 3f;
+
+    private Transform target;
+    private float spawnRate;
+    private float timeAfterSpawn;
+
+    void Start()
+    {
+        timeAfterSpawn = 0f;
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        target = FindObjectOfType<PlayerController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeAfterSpawn += Time.deltaTime;
+        
+        if(timeAfterSpawn>=spawnRate)
+        {
+            timeAfterSpawn = 0f;
+
+            GameObject bullet = Instantiate(bulletPrefeb, transform.position, transform.rotation);
+            bullet.transform.LookAt(target);
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        }
+    }
+}
---
+++ 06/Dodge/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //private Rigidbody playerRigdbody;
+    //public float speed = 8f;
+
+    const float Gravity = 9.81f;
+    public float gravityScale = 1.0f;
+
+
+    private void Start()
+    {
+        //playerRigdbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        /*float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        float xspeed = speed * xInput;
+        float yspeed = speed * yInput;
+
+        Vector3 newVlelocity = new Vector3(xspeed, 0, yspeed);
+        playerRigdbody.velocity = newVlelocity;*/
+
+        Vector3 vector = new Vector3();
+
+        if (Application.isEditor)
+        {
+            vector.x = Input.GetAxis("Horizontal");
+            vector.z = Input.GetAxis("Vertical");
+
+            if(Input.GetKey("z"))
+            {
+                vector.y = 1.0f;
+            }
+            else
+            {
+                vector.y = -1.0f;
+            }
+        }
+        else
+        {
+            vector.x = Input.acceleration.x;
+            vector.y = Input.acceleration.y;
+            vector.z = Input.acceleration.z;
+        }
+
+        Physics.gravity = Gravity * vector.normalized * gravityScale;
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+
+        GameManager gameManeger = FindObjectOfType<GameManager>();
+        gameManeger.EndGame();
+    }
+}
